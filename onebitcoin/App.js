9d987a0dfb6b3163b2a145d21328c771c87cfcec
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-04-05 09:34:05 -0300

    criando pastas e arquivos principais

--- onebitcoin/App.js
+++ onebitcoin/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#000000",
+    alignItems: "center",
+    paddingTop: Platform.OS === "android" ? 40 : 0, //somente aplicado para android
   },
 });
