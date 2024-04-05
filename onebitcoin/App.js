560e1280a0346c93928c6872efadb060e0341385
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-04-05 09:55:20 -0300

    renoameado style para styles e iniciando os códigos do app

--- onebitcoin/App.js
+++ onebitcoin/App.js
@@ -1,12 +1,14 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Platform } from "react-native";
+import { StyleSheet, StatusBar, SafeAreaView, Platform } from "react-native";
+
+import CurrentPrice from "./src/components/CurrentPrice";
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {/* Onde fica as informações de wi-fi, bateria e hora na parte superior da tela */}
+      <StatusBar backgroundColor="#f50d41" barStyle="dark-content" />
+      <CurrentPrice />
     </SafeAreaView>
   );
 }
