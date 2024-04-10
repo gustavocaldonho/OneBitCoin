33a943b9adb00fa6557c164e8e1883ffccc311b7
Gustavo Caldonho <gcaldonho3@gmail.com>
2024-04-10 08:37:08 -0300

    funções de acesso a api implementadas

--- onebitcoin/App.js
+++ onebitcoin/App.js
@@ -6,6 +6,52 @@ import HistoryGraphic from "./src/components/HistoryGraphic";
 import QuotationsList from "./src/components/QuotationsList";
 import QuotationsItems from "./src/components/QuotationsList/QuotationsItems";
 
+function addZero(number) {
+  if (number <= 9) {
+    return "0" + number;
+  }
+  return number;
+}
+
+function url(qtdDays) {
+  const date = new Date();
+  const listLastDays = qtdDays;
+  const end_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+  date.setDate(data.getDate() - listLastDays); //voltando alguns dias (passados como parâmetro)
+  const start_date = `${date.getFullYear()}-${addZero(
+    date.getMonth() + 1
+  )}-${addZero(date.getDate())}`;
+  return `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start_date}&${(end =
+    end_date)}`;
+}
+
+async function getListCoins(url) {
+  let response = await fetch(url);
+  let returnApi = await response.json();
+  let selectListQuotations = returnApi.bpi;
+  const queryCoinsList = Object.keys(selectListQuotations).map((key) => {
+    return {
+      data: key.split("-").reverse().join("/"),
+      valor: selectListQuotations[key],
+    };
+  });
+  let data = queryCoinsList.reverse();
+  return data;
+}
+
+async function getPriceCoinsGraphic(url) {
+  let responseG = await fetch(url);
+  let returnApiG = await responseG.json();
+  let selectListQuotationsG = returnApiG.bpi;
+  const queryCoinsList = Object.keys(selectListQuotationsG).map(
+    (key) => selectListQuotationsG[key]
+  );
+  let dataG = queryCoinsList.reverse();
+  return dataG;
+}
+
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
